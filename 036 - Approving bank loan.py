3e9d61585d94bbe989bf32d567b6e3789344ad16
RodolfoCRS <rodolfocrs.eng@gmail.com>
2022-10-03 19:43:31 -0300

    Python

--- 036 - Approving bank loan.py
+++ 036 - Approving bank loan.py
@@ -2,13 +2,13 @@ print('-=-'*20)
 print(' '*20 + 'Approving bank loan')
 print('-=-'*20)
 
-housevalue = float(input('What is the value of the house? \n $: '))
-salary = float(input('What is the value of your salary? \n $: '))
+housevalue = float(input('What is the value of the house? \n $:'))
+salary = float(input('What is the value of your salary? \n $:'))
 timetopay = int(input('How many years would you like to pay? \n Years: '))
 
 mpay = housevalue / (timetopay*12)
 if mpay >= (salary*0.3):
     print('Unfortunately your loan was \033[0;31m DENIED \033[m\nInstallment value: ${:.2f}'.format(mpay))
-    print('Your salary must be {:.2f} to be \033[0;32m APPROVED \033[m'.format(mpay/0.3))
+    print('Your salary must be {:.2f} to be \033[0;32mAPPROVED \033[m'.format(mpay/0.3))
 else:
     print('Congratulations, your loan was been \033[0;32m APPROVED \033[m\nInstallment value: ${:.2f}'.format(mpay))
